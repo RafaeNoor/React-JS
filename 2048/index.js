1b7e9ceefe28d7c062fc9f350a895a67533cd64c
Abdul Rafae Noor <38834969+RafaeNoor@users.noreply.github.com>
2018-08-20 18:55:39 +0500

    Add files via upload

--- 2048/index.js
+++ 2048/index.js
@@ -30,6 +30,7 @@ class Board extends React.Component {
     let gameBoard = randomBoard();
     this.state = {
       board: gameBoard,
+      score: 0,
     }
     this.handleKeyPress = this.handleKeyPress.bind(this);
   }
@@ -47,6 +48,7 @@ class Board extends React.Component {
       }
       ls.push(e('div',{className:"board-row"},rowDiv))
     }
+    ls.push(e('h2',{},`Score: ${this.state.score}`));
     
     
     return e('div',null
@@ -66,57 +68,23 @@ class Board extends React.Component {
   handleKeyPress(evt){
     let copyBoard = this.state.board.slice();
 
-    /*let newBoard = Array(16).fill(0);
-
-    switch(evt.keyCode){
-      case 65:
-      case 37: 
-        console.log('Left Shift');
-        for(let i = 0;i<16;i++){
-          newBoard[i-i%4] += copyBoard[i];
-        }
-        break;
-      case 87:
-      case 38:
-        console.log('Up Shift');
-        for(let i = 0;i<16;i++){
-          newBoard[i%4] += copyBoard[i];
-        }
-        break;
-      case 68:
-      case 39:
-        console.log('Right Shift');
-        for(let i = 0;i<16;i++){
-          let j = i + (4-i%4)-1;
-          newBoard[j] += copyBoard[i];
-        }
-        break;
-      case 83:
-      case 40:
-        console.log('Down Shift')
-        for(let i = 0;i<16;i++){
-          let j = i;
-          while(j<12){
-            j+=4
-          }
-          newBoard[j] += copyBoard[i];
-        }
-        break;
-      
-    }*/
-
     let newBoard = game2048(copyBoard,evt.keyCode);
+    let updatedScore = -2;
+    newBoard.forEach(val => { updatedScore += val;});
+
     
     let emptySpace = false;
     while(!emptySpace){
       let rand = Math.floor(Math.random()*16);
       if(newBoard[rand] === 0){
+        console.log(`inserting random 2 at ${Math.floor(rand/4)},${rand%4}`);
         newBoard[rand] += 2;
         emptySpace = true;
       }
     }
-    this.setState({
+        this.setState({
       board: newBoard,
+      score: updatedScore,
     });
   }
   
@@ -133,21 +101,25 @@ ReactDOM.render(
 
 
 function game2048(board,keyCode){
-  let newBoard = Array(16).fill(0);
-  newBoard = board;
+  let newBoard = board //Array(16).fill(0);
   switch(keyCode){
     case 65:
     case 37: 
       console.log('Left Shift');
-      for(let i = 15;i>=0;i--){
-        if(i%4!=0){  //non left most column
-          if(board[i] && board[i] == board[i-1]){
-            newBoard[i-1] = board[i]*2;
-            newBoard[i] = 0;
-            board[i] = 0;
-          } else if(board[i-1] == 0) {
-            newBoard[i-1] = board[i];
-            board[i] = 0;
+      for(let i =0;i<16;i++){
+        let leftLim = i - (i%4);
+        let tempPos = i;
+        for(let j = i-1; j>= leftLim; j--){
+          if(newBoard[j] && newBoard[j] == newBoard[tempPos]){
+            newBoard[j] *= 2;
+            newBoard[tempPos] = 0;
+            break;
+          } else if(newBoard[j]){ // value is different
+            break;
+          } else { // if zero valued
+            newBoard[j] = newBoard[tempPos];
+            newBoard[tempPos] = 0;
+            tempPos = j;
           }
         }
       }
@@ -155,33 +127,43 @@ function game2048(board,keyCode){
     case 87:
     case 38:
       console.log('Up Shift');
-      for(let i = 15;i>=0;i--){
-        if(i>3){
-          if(board[i] && board[i-4] == board[i]){
-            newBoard[i-4] = board[i-4]*2;
-            newBoard[i] = 0;
-            board[i] = 0;
-
-          } else if(board[i-4] == 0){
-            newBoard[i-4] = board[i];
-            board[i] = 0;
+      for(let i = 0; i<16;i++){
+        let upLim = i%4;
+        let tempPos = i;
+        for(let j = i-4; j >= upLim; j-=4){
+          
+          if(newBoard[j] && newBoard[j] == newBoard[tempPos]){
+            newBoard[j] *= 2;
+            newBoard[tempPos] = 0;
+            break;
+          } else if(newBoard[j]){ // value is different
+            break;
+          } else { // if zero valued
+            newBoard[j] = newBoard[tempPos];
+            newBoard[tempPos] = 0;
+            tempPos = j;
           }
+
         }
       }
       break;
     case 68:
     case 39:
       console.log('Right Shift');
-      for(let i = 0;i<16;i++){
-        let j = i + (4-i%4)-1;
-        if(i != j){// not right most column
-          if(board[i] && board[i+1] == board[i]){
-            newBoard[i+1] = board[i+1]*2;
-            //newBoard[i] = 0;
-            board[i] = 0; 
-          } else if( board[i+1] == 0){
-            newBoard[i+1] = board[i];
-            board[i] = 0;
+      for(let i=15;i>=0;i--){
+        let rightLim = i - (i%4) +3;
+        let tempPos = i;
+        for(let j = i+1; j <= rightLim; j++){
+          if(newBoard[j] && newBoard[j] == newBoard[tempPos]){
+            newBoard[j] *= 2;
+            newBoard[tempPos] = 0;
+            break;
+          } else if(newBoard[j]){ // value is different
+            break;
+          } else { // if zero valued
+            newBoard[j] = newBoard[tempPos];
+            newBoard[tempPos] = 0;
+            tempPos = j;
           }
         }
       }
@@ -189,20 +171,25 @@ function game2048(board,keyCode){
     case 83:
     case 40:
       console.log('Down Shift')
-      for(let i = 0;i<16;i++){
-        //let j = i;
-        //while(j<12){
-        //  j+=4
-        //}
-        if(i < 12){
-          if(board[i] && board[i+4] == board[i]){
-            newBoard[i+4] = board[i+4]*2;
-            board[i] = 0;
-            newBoard[i] = 0;
-          } else if( board[i+4] == 0){
-            newBoard[i+4] = board[i];
-            board[i] = 0;
+      for(let i = 15;i>=0;i--){
+        let downLim = i;
+        let tempPos = i;
+        while(downLim <12){
+          downLim += 4;
+        }
+        for(let j = i+4; j <= downLim; j += 4){
+          if(newBoard[j] && newBoard[j] == newBoard[tempPos]){
+            newBoard[j] *= 2;
+            newBoard[tempPos] = 0;
+            break;
+          } else if(newBoard[j]){ // value is different
+            break;
+          } else { // if zero valued
+            newBoard[j] = newBoard[tempPos];
+            newBoard[tempPos] = 0;
+            tempPos = j;
           }
+
         }
       }
       break;
